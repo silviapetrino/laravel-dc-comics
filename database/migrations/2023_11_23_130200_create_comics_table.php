d0481ce97368f73809f1bc21c7095d3398a3c985
silviapetrino <petrino.silvia@gmail.com>
2023-11-23 14:31:06 +0100

    [add] db, migrations, main layout, homepage route

---
+++ database/migrations/2023_11_23_130200_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->string('description');
+            $table->string('thumb', 200);
+            $table->string('price');
+            $table->string('series');
+            $table->string('sale_date');
+            $table->string('type');
+            $table->text('artists');
+            $table->text('writers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
